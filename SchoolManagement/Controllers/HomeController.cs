d47694ab695dbbb1b5d0fd2ff9148abdbfe69cfb
Ornella Fanais <ornella.fanais@avanade.com>
2022-01-20 16:35:50 +0100

    add courses view and controller

--- SchoolManagement/Controllers/HomeController.cs
+++ SchoolManagement/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace SchoolManagement.Controllers
 {
     public class HomeController : Controller
     {
+        //Every method you have here you should have a corresponding
+        //view file and that is what this returns.
+        //When you send a request for index from the home controller
+        //it will search through the view folder and then look for a follow the
+        //corresponding name or home and then it will look for a controller with a view
         public ActionResult Index()
         {
             return View();
@@ -24,6 +29,11 @@ namespace SchoolManagement.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+        //New view
+        public ActionResult TestView()
+        {
             return View();
         }
     }
